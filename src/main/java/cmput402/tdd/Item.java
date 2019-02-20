e543eb90efe48b6443fda9c64249bbafbe3fb69d
Henry Tang <hktang@ualberta.ca>
2019-02-20 12:40:56 -0700

    RED: Added Item structure and tests

--- src/main/java/cmput402/tdd/Item.java
+++ src/main/java/cmput402/tdd/Item.java
@@ -1,4 +1,30 @@
 package cmput402.tdd;
 
 public class Item {
+
+    private String name;
+
+    private float cost;
+
+    public Item(String name, Float cost) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+    }
+
+    public float getCost() {
+        return 0;
+    }
+
+    public void setCost(float cost) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
 }
--- src/test/java/cmput402/tdd/ItemTest.java
+++ src/test/java/cmput402/tdd/ItemTest.java
@@ -1,6 +1,37 @@
 package cmput402.tdd;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class ItemTest extends TestCase {
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    private Item createTestItem(String name, Float cost) {
+        return new Item(name, cost);
+    }
+
+    @Test
+    public void testGetName() {
+        Item tuna = createTestItem("Tuna", 2.00f);
+        assertEquals("Tuna", tuna.getName());
+    }
+
+    @Test
+    public void testGetCost() {
+        Item salmon = createTestItem("Salmon", 5.00f);
+        assertEquals(salmon.getCost(), (5.00f), 0.0);
+    }
+
+    @Test
+    public void TestNotEquals() {
+        Item tuna = createTestItem("Tuna", 2.00f);
+        Item salmon = createTestItem("Salmon", 5.00f);
+        assertNotEquals(tuna, salmon);
+    }
+
+    @Test
+    public void TestEquals() {
+        Item tuna = createTestItem("Tuna", 2.00f);
+        assertTrue(tuna.equals(tuna));
+    }
 }
