49dcbc66cfa1888528460cf2959691eb26d3180e
Henry Tang <hktang@ualberta.ca>
2019-02-20 14:41:29 -0700

    RED: Added Item structure and tests

--- src/main/java/cmput402/tdd/Item.java
+++ src/main/java/cmput402/tdd/Item.java
@@ -1,4 +1,41 @@
 package cmput402.tdd;
 
 public class Item {
+
+    private String name;
+
+    private float cost;
+
+    public Item(String name) {
+    }
+
+    public Item(String name, float cost) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+    }
+
+    public float getCost() {
+        return 0;
+    }
+
+    public void setCost(float cost) {
+
+    }
+
+    /**
+     * We want to implement our own equals method to specifically check that the name and cost are equal
+     * @param o
+     *  A Item object
+     * @return
+     *  Whether the object o has the same name and cost as this
+     */
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
 }
--- src/test/java/cmput402/tdd/ItemTest.java
+++ src/test/java/cmput402/tdd/ItemTest.java
@@ -2,10 +2,79 @@ package cmput402.tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class ItemTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNegativeCost() {
+        new Item("Tuna", -5.00f);
+    }
+
     @Test
-    public void testItem() {
-        assert true;
+    public void testGetName() {
+        Item tuna = new Item("Tuna");
+        assertEquals("Tuna", tuna.getName());
+    }
+
+    @Test
+    public void testGetCost() {
+        Item salmon = new Item("Salmon", 5.00f);
+        assertEquals(salmon.getCost(), (5.00f), 0.0);
+    }
+
+    @Test
+    public void testSetName() {
+        Item salmon = new Item("Tuna");
+        assertEquals("Tuna", salmon.getName());
+        salmon.setName("Salmon");
+        assertEquals("Salmon", salmon.getName());
+    }
+
+    @Test
+    public void testSetCost() {
+        Item tuna = new Item("Tuna", 5.00f);
+        assertEquals(5.00f, tuna.getCost(), 0);
+        tuna.setCost(10.00f);
+        assertEquals(10.00f, tuna.getCost(), 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNegativeCost() {
+        Item tuna = new Item("Tuna");
+        tuna.setCost(-5.00f);
+    }
+
+    @Test
+    public void TestEquals() {
+        Item tuna1 = new Item("Tuna", 2.00f);
+        Item tuna2 = new Item("Tuna", 2.00f);
+        assertTrue(tuna1.equals(tuna2));
+    }
+    @Test
+    public void TestEqualsSameObject() {
+        Item tuna = new Item("Tuna", 2.00f);
+        assertTrue(tuna.equals(tuna));
+    }
+
+    @Test
+    public void TestNotEqualsName() {
+        Item tuna = new Item("Tuna", 2.00f);
+        Item salmon = new Item("Salmon", 2.00f);
+        assertFalse(tuna.equals(salmon));
+    }
+
+    @Test
+    public void TestEqualsNull() {
+        Item tuna = new Item("Tuna", 2.00f);
+        Person person = new Person();
+        assertFalse(tuna.equals(person));
+    }
+
+    @Test
+    public void TestNotEqualsCost() {
+        Item tuna1 = new Item("Tuna", 2.00f);
+        Item tuna2 = new Item("Tuna", 5.00f);
+        assertFalse(tuna1.equals(tuna2));
     }
 }
