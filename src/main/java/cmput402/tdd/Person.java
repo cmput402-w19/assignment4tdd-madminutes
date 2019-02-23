d7c874d140c585b84f59d46607d6edae73a49b65
Henry Tang <hktang@ualberta.ca>
2019-02-22 21:55:22 -0700

    GREEN: Added implementation for person name

--- src/main/java/cmput402/tdd/Person.java
+++ src/main/java/cmput402/tdd/Person.java
@@ -5,10 +5,10 @@ public class Person {
     private String name;
 
     public Person(String name) {
-
+        this.name = name;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 }
