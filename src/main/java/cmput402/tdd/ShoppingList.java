183643570235b581cb898d8cba6fd3cbc89b0154
Henry Tang <hktang@ualberta.ca>
2019-02-22 18:28:56 -0700

    GREEN: Added shopping list name implementation

--- src/main/java/cmput402/tdd/ShoppingList.java
+++ src/main/java/cmput402/tdd/ShoppingList.java
@@ -5,15 +5,15 @@ public class ShoppingList {
     private String name;
 
     public ShoppingList(String name){
-
+        this.name = name;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
-
+        this.name = name;
     }
 
 }
