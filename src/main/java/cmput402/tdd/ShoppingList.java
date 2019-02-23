e307593e96b4c9113387c3546de371cb1ad2a9a6
Henry Tang <hktang@ualberta.ca>
2019-02-22 18:28:21 -0700

    RED: Added shopping list name tests and structure

--- src/main/java/cmput402/tdd/ShoppingList.java
+++ src/main/java/cmput402/tdd/ShoppingList.java
@@ -1,4 +1,19 @@
 package cmput402.tdd;
 
 public class ShoppingList {
+
+    private String name;
+
+    public ShoppingList(String name){
+
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+
+    }
+
 }
--- src/test/java/cmput402/tdd/ShoppingListTest.java
+++ src/test/java/cmput402/tdd/ShoppingListTest.java
@@ -3,10 +3,19 @@ package cmput402.tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ShoppingListTest {
 
     @Test
-    public void testShoppingList() {
-        assert true;
+    public void testName() {
+        ShoppingList list1 = new ShoppingList("list1");
+
+        // Test1: getName
+        assertEquals("list1", list1.getName());
+
+        // Test2: test setName
+        list1.setName("list2");
+        assertEquals("list2", list1.getName());
     }
 }
