f19d46d6d437a6c9934e89ec4e175dcc9a2e0781
forgeno <ima@ualberta.ca>
2019-02-22 20:50:41 -0700

    RED: Added recipe name tests and structure

--- src/test/java/cmput402/tdd/RecipeTest.java
+++ src/test/java/cmput402/tdd/RecipeTest.java
@@ -1,12 +1,18 @@
 package cmput402.tdd;
 
 
+import junit.framework.TestCase;
 import org.junit.Test;
 
-public class RecipeTest {
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+public class RecipeTest extends TestCase {
     @Test
-    public void testRecipe() {
+    public void testRecipeName() {
+        //Test 1: Check initial name on creation of list
+        Recipe recipe = new Recipe("recipe1");
+        assertTrue(recipe.getName().equals("recipe1"));
 
     }
 }
