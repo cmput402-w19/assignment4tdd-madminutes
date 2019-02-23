f3a139ee698139912a10b379cc98e23c54df5fb7
forgeno <ima@ualberta.ca>
2019-02-22 20:50:41 -0700

    RED: Added recipe name tests and structure

--- src/main/java/cmput402/tdd/Recipe.java
+++ src/main/java/cmput402/tdd/Recipe.java
@@ -1,6 +1,17 @@
 package cmput402.tdd;
+import org.apache.commons.collections4.map.LinkedMap;
+
+import java.util.Map;
 
 public class Recipe {
+
+    private String name = "";
+    public LinkedMap<Item, Integer> items = new LinkedMap<Item, Integer>();
+
+    public Recipe(String name) {
+
+    }
+
     public String getName() {
         return null;
     }
