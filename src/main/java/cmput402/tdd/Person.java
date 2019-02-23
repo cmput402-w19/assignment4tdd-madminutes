dac501bb1b5a93a3f31fabc2d56f9e90ebb4f1d4
Henry Tang <hktang@ualberta.ca>
2019-02-22 22:08:48 -0700

    RED: Added tests for adding a shopping list to a person

--- src/main/java/cmput402/tdd/Person.java
+++ src/main/java/cmput402/tdd/Person.java
@@ -1,8 +1,12 @@
 package cmput402.tdd;
 
+import org.apache.commons.collections4.map.LinkedMap;
+
 public class Person {
 
     private String name;
+
+    private LinkedMap<String, ShoppingList> shoppingLists;
 
     public Person(String name) {
         this.name = name;
@@ -10,5 +14,13 @@ public class Person {
 
     public String getName() {
         return name;
+    }
+
+    public LinkedMap<String, ShoppingList> getShoppingLists() {
+        return null;
+    }
+
+    public boolean addShoppingList(ShoppingList shoppingList) {
+        return false;
     }
 }
--- src/test/java/cmput402/tdd/PersonTest.java
+++ src/test/java/cmput402/tdd/PersonTest.java
@@ -2,7 +2,9 @@ package cmput402.tdd;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PersonTest {
 
@@ -10,5 +12,21 @@ public class PersonTest {
     public void testGetName() {
         Person person = new Person("John");
         assertEquals("John", person.getName());
+    }
+
+    @Test
+    public void testAddShoppingList() {
+        Person person = new Person("John");
+
+        assertEquals(0, person.getShoppingLists().size());
+
+        ShoppingList mockShoppingList = mock(ShoppingList.class);
+        when(mockShoppingList.getName()).thenReturn("Monday Shopping List");
+
+        assertTrue(person.addShoppingList(mockShoppingList));
+        assertEquals(1, person.getShoppingLists().size());
+
+        assertFalse(person.addShoppingList(mockShoppingList));
+        assertEquals(1, person.getShoppingLists().size());
     }
 }
