c7d8c0f812be2d0d76bce870e63dac164fe0629b
Henry Tang <hktang@ualberta.ca>
2019-02-22 21:54:33 -0700

    RED: added person name test and structure

--- src/main/java/cmput402/tdd/Person.java
+++ src/main/java/cmput402/tdd/Person.java
@@ -1,4 +1,14 @@
 package cmput402.tdd;
 
 public class Person {
+
+    private String name;
+
+    public Person(String name) {
+
+    }
+
+    public String getName() {
+        return null;
+    }
 }
--- src/test/java/cmput402/tdd/PersonTest.java
+++ src/test/java/cmput402/tdd/PersonTest.java
@@ -2,10 +2,13 @@ package cmput402.tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest {
 
     @Test
-    public void testPerson() {
-        assert true;
+    public void testGetName() {
+        Person person = new Person("John");
+        assertEquals("John", person.getName());
     }
 }
