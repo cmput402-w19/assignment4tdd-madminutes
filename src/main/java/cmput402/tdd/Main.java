4ad58b1e4cffeb340fc5207184c8187c62f616a7
Henry Tang <hktang@ualberta.ca>
2019-02-24 17:27:34 -0700

    Moved Person options loop into select person loop

--- src/main/java/cmput402/tdd/Main.java
+++ src/main/java/cmput402/tdd/Main.java
@@ -14,7 +14,7 @@ public class Main {
         while (person == null && !quit) {
             System.out.println("Choose from these choices");
             System.out.println("-------------------------\n");
-            System.out.println("1 - Find a Person");
+            System.out.println("1 - Select a Person");
             System.out.println("2 - Create a Person");
             System.out.println("3 - Quit");
 
@@ -24,6 +24,43 @@ public class Main {
                 case 1:
                     try {
                         person = app.getPerson(scanner, false);
+                        // Loop to do actions on that person
+                        while (!quit) {
+                            System.out.println("Choose from these choices");
+                            System.out.println("-------------------------\n");
+                            System.out.println("1 - Create a Shopping List");
+                            System.out.println("2 - Edit a Shopping List");
+                            System.out.println("3 - Create a Recipe");
+                            System.out.println("4 - Edit a Recipe");
+                            System.out.println("5 - Quit");
+
+                            int option = scanner.nextInt();
+
+                            try {
+                                switch (option) {
+                                    case 1:
+                                        app.createShoppingList(scanner);
+                                        break;
+                                    case 2:
+                                        ShoppingList shoppingList = app.getShoppingList(scanner, person);
+                                        app.editShoppingList(scanner, shoppingList);
+                                        break;
+                                    case 3:
+                                        // app.createRecipe(scanner);
+                                        break;
+                                    case 4:
+                                        // app.editRecipe(scanner);
+                                        break;
+                                    case 5:
+                                        quit = true;
+                                        break;
+                                    default:
+                                        System.out.println("Please input a valid option.");
+                                }
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
@@ -40,44 +77,6 @@ public class Main {
                     break;
                 default:
                     System.out.println("Please input a valid option.");
-            }
-        }
-
-        // Loop to do actions on that person
-        while (!quit) {
-            System.out.println("Choose from these choices");
-            System.out.println("-------------------------\n");
-            System.out.println("1 - Create a Shopping List");
-            System.out.println("2 - Edit a Shopping List");
-            System.out.println("3 - Create a Recipe");
-            System.out.println("4 - Edit a Recipe");
-            System.out.println("5 - Quit");
-
-            int choice = scanner.nextInt();
-
-            try {
-                switch (choice) {
-                    case 1:
-                        app.createShoppingList(scanner);
-                        break;
-                    case 2:
-                        ShoppingList shoppingList = app.getShoppingList(scanner, person);
-                        app.editShoppingList(scanner, shoppingList);
-                        break;
-                    case 3:
-                        // app.createRecipe(scanner);
-                        break;
-                    case 4:
-                        // app.editRecipe(scanner);
-                        break;
-                    case 5:
-                        quit = true;
-                        break;
-                    default:
-                        System.out.println("Please input a valid option.");
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
         }
     }
